3c661745ce7dd0df34c34f362e4af167f3402662
Chris <cgamel22@student.aau.dk>
2023-07-19 17:27:49 +0200

    contrast tool

--- dimensions.py
+++ dimensions.py
@@ -11,25 +11,41 @@ class Mask():
         #self.blacken_non_white()
         self.contrast()
 
-    def contrast(self):
         alpha = 2
         beta = 10
+        w_range = 102
 
         def update_contrast(_):
-            nonlocal alpha, beta
+            nonlocal alpha, beta, w_range
 
             alpha = cv2.getTrackbarPos('alpha', 'Contrast')
             beta = cv2.getTrackbarPos('beta', 'Contrast')
-            print(alpha, beta)
+            w_range = cv2.getTrackbarPos('w_range', 'Contrast')
+            print(alpha, beta, w_range)
 
         cv2.namedWindow("Contrast")
         cv2.createTrackbar('alpha', "Contrast", 0, 50, update_contrast)
         cv2.createTrackbar('beta', "Contrast", 0, 254, update_contrast)
+        cv2.createTrackbar('w_range', "Contrast", 0, 400, update_contrast)
 
         while True:
-            contrast = cv2.convertScaleAbs(self.image, alpha/10, beta-127)
-            resize_c = self.ResizeWithAspectRatio(contrast, width=1000)
+            contrast = cv2.convertScaleAbs(self.image, alpha, beta) # 50, 6
+            gray = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
 
+            mask = cv2.threshold(gray, w_range, 255, cv2.THRESH_BINARY_INV)[1]  # #2 = white range
+
+            blackened_image = cv2.bitwise_and(self.image, self.image, mask=mask)
+
+            gray_blackened = cv2.cvtColor(blackened_image, cv2.COLOR_BGR2GRAY)
+            _, thresholded = cv2.threshold(gray_blackened, 1, 255, cv2.THRESH_BINARY)
+            contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            total_area = sum(cv2.contourArea(contour) for contour in contours)
+
+            #cv2.imshow("Blackened Image", gray)
+            
+            resize_c = self.ResizeWithAspectRatio(gray_blackened, width=1000)
+            
+    
             cv2.imshow("Contrast", resize_c)
             
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -40,7 +56,7 @@ class Mask():
 
         
     def blacken_non_white(self):
-        
+        contrast = cv2.convertScaleAbs(self.image, 33, 122)
         
         resize_o = self.ResizeWithAspectRatio(self.image, width=1000)
         
@@ -48,7 +64,7 @@ class Mask():
         cv2.imshow("original", resize_o)
         
         
-        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
 
         # Create a mask for pixels within the white range
         mask = cv2.threshold(gray, 105, 255, cv2.THRESH_BINARY_INV)[1]  # #2 = white range
@@ -61,23 +77,12 @@ class Mask():
         contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         total_area = sum(cv2.contourArea(contour) for contour in contours)
 
-        # cv2.imshow("Blackened Image", blackened_image)
+        cv2.imshow("Blackened Image", gray)
         print("Total area of blackened regions:", total_area)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    # def alpha(self, value, name, window): #onchange
-    #     self.alpha_v = value  # 0<a<1
-    #     self.show(name, window)
-
-
-    # def beta(self, value, name, window): #onchange
-    #     self.beta_v = value  # -127, 127
-    #     self.show(name, window)
-
-    # def show(self, name, window):
-    #     cv2.imshow(f"{name}", window)
     def nothing(self, x):
         pass
 
