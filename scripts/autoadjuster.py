0dc85c6ead87f33fea64720f94723d6cf78a50ed
ChristofferGamel <christoffer.gamel@gmail.com>
2023-07-29 10:36:50 +0200

    edit for r+l capabilities

--- scripts/autoadjuster.py
+++ scripts/autoadjuster.py
@@ -1,70 +1,77 @@
 import cv2
 import numpy as np
+import math
+from trianglesolver import solve, degree
+from right_triangle import RightTriangle
 
 
 
-class Tools():
-    def __init__(self) -> None:
-        image_path = "./captured_images/right.jpg"
+
+class Mask():
+    def __init__(self, image_path, image_name, alpha, beta, kernel_iterations, kernel_size, C)-> None:
+        # Image properties
+        
         self.image = cv2.imread(image_path)
+        self.image_name = image_name
+        self.alpha_v = 0
+        self.beta_v = 0
         self.image_height = self.image.shape[0]
         self.image_width = self.image.shape[1]
-        self.contrastTool()
 
-    def contrastTool(self):
-        alpha = 2
-        beta = 10
-        k_size = 1
-        k_iterations = 8
-        C = 4
+        # Camera properties
+        self.camera_angle = 78 #degrees
 
+        # Image adjustments:
+        self.alpha = 0.4645669291338583
+        self.beta = 48
+        self.kernel_iterations = 2
+        self.kernel_size = 4
+        self.C = 4
 
+        self.final_image()
 
-        def update_contrast(_):
-            nonlocal alpha, beta, k_iterations, k_size, C
+    def contrast(self, image, alpha, beta): #0.8110236220472441, 100
+        contrast = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+        return contrast
+    
+    def thresholding(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_8bit = cv2.convertScaleAbs(gray)
+        th2 = cv2.adaptiveThreshold(gray_8bit, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 4)
+        return th2
 
-            alpha = cv2.getTrackbarPos('alpha', 'Contrast') / 127.0
-            beta = cv2.getTrackbarPos('beta', 'Contrast') - 100
-            k_size = cv2.getTrackbarPos('kernel_size', 'Contrast')
-            k_iterations = cv2.getTrackbarPos('kernel_iterations', 'Contrast')
-            
-            C = cv2.getTrackbarPos('C', 'Contrast')
-            print(f"Alpha: {alpha}, Beta: {beta}, kernel size: {k_size}, Kernel iterations: {k_iterations}")
-
-        cv2.namedWindow("Contrast")
+    def erosion(self, image):
+        kernel = np.ones((self.kernel_size,self.kernel_size),np.uint8)
+        er = cv2.erode(image,kernel,iterations = self.kernel_iterations)
+        return er
+    
+    def contour(self, image):
+        contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
         
-        cv2.createTrackbar('alpha', "Contrast", 0, 127, update_contrast) #[0,127]
-        cv2.createTrackbar('beta', "Contrast", 0, 200, update_contrast) #[-100,100]
-        cv2.createTrackbar('kernel_size', "Contrast", 0, 10, update_contrast) 
-        cv2.createTrackbar('kernel_iterations', "Contrast", 0, 10, update_contrast)
+        con = self.image.copy() 
+        cv2.drawContours(con, contours, 3, (0, 255, 0), 3)
         
-        cv2.createTrackbar('C', "Contrast", 0, 10, update_contrast)
+        largest_contour = max(contours, key=cv2.contourArea)
+        contours_without_largest = [contour for contour in contours if contour is not largest_contour]
+        second_largest_contour = max(contours_without_largest, key=cv2.contourArea)
+        # print(second_largest_contour)
+        self.find_extremes(second_largest_contour)
 
-        while True:
-            contrast = cv2.convertScaleAbs(self.image, alpha=alpha, beta=beta)
-            gray = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
-            gray_8bit = cv2.convertScaleAbs(gray)
-            th2 = cv2.adaptiveThreshold(gray_8bit, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, C)
-            kernel = np.ones((k_size,k_size),np.uint8)
-            er = cv2.erode(th2,kernel,iterations = k_iterations)
-            ret, thresh = cv2.threshold(er, 150, 255, cv2.THRESH_BINARY)
-            self.image_copy = self.image.copy()
-            #resize_c = self.ResizeWithAspectRatio(self.image_copy, width=300)
-            contoured = self.contour(thresh)
-            cv2.imshow("Contrast", contoured)
-
+        image_with_polygon = self.image.copy() 
+        cv2.line(image_with_polygon, (self.max_x,self.min_y), (self.max_x,self.max_y), (0, 255, 0), 3)
+        cv2.line(image_with_polygon, (self.min_x,self.max_y), (self.max_x,self.max_y), (0, 255, 0), 3)
+        cv2.line(image_with_polygon, (self.min_x,self.min_y), (self.min_x,self.max_y), (0, 255, 0), 3)
+        cv2.line(image_with_polygon, (self.min_x,self.min_y), (self.max_x,self.min_y), (0, 255, 0), 3)
+        cv2.drawContours(image_with_polygon, second_largest_contour, -1, (0, 255, 0), thickness=2)
         
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        
-        cv2.destroyAllWindows()
+        return image_with_polygon
     
     def find_extremes(self, list):
         self.max_x = 0
         self.max_y = 0
         self.min_y = self.image_height
         self.min_x = self.image_width
-        
+        print(f"Height: {self.image_height}, Width: {self.image_width}")
         
         for m_x in range(len(list)):
             if(list[m_x][0][0] > self.max_x):
@@ -83,53 +90,207 @@ class Tools():
             if list[mi_x][0][0] < self.min_x:
                 self.min_x = list[mi_x][0][0]
                 self.min_x_set = list[mi_x][0]
-    def contour(self, image):
-        
-        contours, hierarchy = cv2.findContours(image=image, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-                                            
-        # draw contours on the original image
-        
-        
-        try:
-            largest_contour = max(contours, key=cv2.contourArea)
-            contours_without_largest = [contour for contour in contours if contour is not largest_contour]
-            second_largest_contour = max(contours_without_largest, key=cv2.contourArea)
-            self.find_extremes(second_largest_contour)
-            cv2.line(self.image_copy, (self.max_x,self.min_y), (self.max_x,self.max_y), (255, 0, 0), 3)
-            cv2.line(self.image_copy, (self.min_x,self.max_y), (self.max_x,self.max_y), (255, 0, 0), 3)
-            cv2.line(self.image_copy, (self.min_x,self.min_y), (self.min_x,self.max_y), (255, 0, 0), 3)
-            cv2.line(self.image_copy, (self.min_x,self.min_y), (self.max_x,self.min_y), (255, 0, 0), 3)
-        except:
-            print("Max hit")
-        
-        cv2.drawContours(self.image_copy, contours, -1, (0, 255, 0), 2, cv2.LINE_AA)
 
-        for contour in contours:
-            # Approximate the contour with a polygon
-            epsilon = 0.14 * cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, epsilon, True)
 
-            # Draw the polygon
-            cv2.polylines(self.image_copy, [approx], True, (0, 255, 0), 2)
-        return self.image_copy
 
-        
+        print("Max x set: ",self.max_x_set)
+        print("Max y set: ",self.max_y_set)
+        print("Min x set: ",self.min_x_set)
+        print("min y set: ",self.min_y_set)
     
-    def ResizeWithAspectRatio(self, image, width=None, height=None, inter=cv2.INTER_AREA):
-        dim = None
-        (h, w) = image.shape[:2]
+    def angle(self, orientation): # angle from leftmost fov to r or l edge
+        if orientation=="l": #left edge of image
+            distance_pixels = self.min_x_set[0]
+            angle = (distance_pixels/self.image_width) * self.camera_angle
 
-        if width is None and height is None:
-            return image
-        if width is None:
-            r = height / float(h)
-            dim = (int(w * r), height)
-        else:
-            r = width / float(w)
-            dim = (width, int(h * r))
+        elif orientation=="r": #right edge of image
+            distance_pixels = self.max_x_set[0]
+            angle = (distance_pixels/self.image_width) * self.camera_angle
+        elif orientation=="test":
+            object_center_x = self.max_x_set[0] - self.min_x_set[0]
+            angle = self.calculate_angle(self.image_width, object_center_x, 360, self.camera_angle)
+        
+        return(angle)
+    def calculate_angle(self, image_width, point, image_center_x, FOV_x):
+        angle_rad = math.atan((point - image_center_x) * math.tan(self.rad_to_deg(FOV_x) / 2) / (image_width / 2))
+        angle_deg = self.rad_to_deg(angle_rad)
+        return angle_deg
+    
+    def calculate_angle2(self, image_width, point, FOV_x):
+        angle_rad = math.atan((2*point/image_width) * FOV_x/image_width)
+        angle_deg = self.rad_to_deg(angle_rad)
+        return angle_deg
+    def calculate_angle_y(self, image_height, point_y, FOV_y):
+        angle_rad = math.atan((2 * point_y / image_height - 1) * math.tan(math.radians(FOV_y) / 2))
+        angle_deg = math.degrees(angle_rad)
+        return angle_deg
+    
+    def rad_to_deg(self, rad):
+        return(rad*(180/math.pi))
+    def deg_to_rad(self, deg):
+        return((deg * math.pi)/180)
+    def properties(self):
+        dict = {"image_width":self.image_width, 
+                "image_height":self.image_height,
+                "y_min_set":self.min_y_set,
+                "x_min_set":self.min_x_set,
+                "y_max_set":self.max_y_set,
+                "x_max_set":self.max_x_set,
+                "cam_fov":self.camera_angle}
+        return dict
+    
+    def final_image(self):
+        contrasted = self.contrast(self.image, self.alpha, self.beta)
+        thresholded = self.thresholding(contrasted)
+        eroded = self.erosion(thresholded)
+        contoured = self.contour(eroded)
+        while True:
+            cv2.imshow(f"{self.image_name}", contoured)
+            key = cv2.waitKey(1) & 0xFF
 
-        return cv2.resize(image, dim, interpolation=inter)
+            
+            if key == ord("q"):
+                break
+        cv2.destroyAllWindows()
+        return(contoured)
+    
+    def nothing(self, x):
+        pass
+
+#  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+
+class Dimensions():
+    def __init__(self) -> None:
+        left_path = "./captured_images/left.jpg"
+        right_path = "./captured_images/right.jpg"
+        alpha = 0.4645669291338583
+        beta = 48
+        kernel_i = 2
+        kernel_size = 4
+        C = 4
+        self.left_properties = Mask(left_path, "Left", alpha, beta, kernel_i, kernel_size, C) #front
+        self.right_properties = Mask(right_path, "Right", alpha, beta, kernel_i, kernel_size, C) #side
+        
+        
+        self.main()
+    
+    def positive(self,num):
+        return (math.sqrt((num)**2))
+
+
+    def common_point(self):
+        # Image properties:
+        dist_betw_cams = 40.54
+        self.left_image_properties = self.left_properties.properties()
+        self.right_image_properties = self.right_properties.properties()
+        print("l/r")
+
+        self.left_image_width = self.left_image_properties["image_width"]
+        self.right_image_width = self.right_image_properties["image_width"]
+        print("l/r: Width:",self.left_image_width, self.right_image_width)
+        
+        self.left_center = self.left_image_width / 2
+        self.right_center = self.right_image_width / 2
+        print("l/r: Center:",self.left_center, self.right_center)
+        
+        
+        self.left_fov = self.left_image_properties["cam_fov"]
+        self.right_fov = self.right_image_properties["cam_fov"]
+        print("l/r FOV: ",self.left_fov,self.right_fov)
+
+        # Contoured values
+        self.left_cam_min_x = self.left_image_properties["x_min_set"][0]
+        self.left_cam_max_x = self.left_image_properties["x_max_set"][0]
+        print("left_max: ",self.left_cam_max_x)
+
+
+        self.right_cam_min_x = self.right_image_properties["x_min_set"][0]
+        self.right_cam_max_x = self.right_image_properties["x_max_set"][0]
+        print("right_min: ",)
+
+        
+        # Angles
+        left_cam_angle_to_right_point = self.left_properties.calculate_angle2(self.left_image_width, self.left_cam_max_x, self.left_fov)
+        right_cam_angle_to_left_point = self.right_properties.calculate_angle2(self.right_image_width, self.right_cam_min_x, self.right_fov)
+
+        A = 90 - 45 - math.sqrt((left_cam_angle_to_right_point)**2)
+        B = 90 - 45 - math.sqrt((right_cam_angle_to_left_point)**2)
+        print("ab: ",left_cam_angle_to_right_point,right_cam_angle_to_left_point)
+        c = dist_betw_cams
+
+        a,b,c,A,B,C = solve(c=c, A=A*degree, B=B*degree)
+        self.b = b
+
+        print(a,b)
+
+    def width(self): # front / left camera
+        image_properties = self.left_properties.properties()
+        right = self.left_properties.calculate_angle2(self.left_image_width, self.left_cam_max_x, self.left_fov)
+        left = self.left_properties.calculate_angle2(self.left_image_width, self.left_cam_min_x, self.left_fov)
+        object_angle = self.positive(right) + self.positive(left)
+
+        # Method 1 assuming orthogonal placement of object
+        C = object_angle
+        A = 90 - self.positive(right)
+        B = 90 - self.positive(left)
+        b = self.b
+        print(f"C: {C}, A: {A}, B: {B}, b:{b}")
+
+        a,b,c,A,B,C = solve(C=C*degree,B=B*degree,b=b)
+        self.triangle_height = math.sin(A)*b
+
+        print(c)
+        return c
+
+    def depth(self): # length
+        image_properties = self.right_properties.properties()
+        right = self.right_properties.calculate_angle2(self.right_image_width, self.right_cam_max_x, self.right_fov)
+        left = self.left_properties.calculate_angle2(self.right_image_width, self.right_cam_min_x, self.right_fov)
+        object_angle = self.positive(right) + self.positive(left)
+
+        # Method 1 assuming orthogonal placement of object
+        C = object_angle
+        A = 90 - self.positive(right)
+        B = 90 - self.positive(left)
+        b = self.b
+        print(f"C: {C}, A: {A}, B: {B}, b:{b}")
+
+        a,b,c,A,B,C = solve(C=C*degree,B=B*degree,b=b)
+        print("depth: ",c)
+        return c
+          
+    
+    def height(self): # 
+        dist_to_center = self.triangle_height
+        max_y = self.left_image_properties["y_max_set"][1]
+        min_y = self.left_image_properties["y_min_set"][1]
+        height = self.left_image_properties["image_height"]
+        center = height/2
+        angle1 = self.left_properties.calculate_angle_y(height, max_y, self.left_fov)
+        angle2 = self.left_properties.calculate_angle_y(height, min_y, self.left_fov)
+        
+        angle_sum = self.positive(angle1) + self.positive(angle2)   
+        
+
+
+        return
+    def deg_to_rad(self, deg):
+        return((deg * math.pi)/180)
+    
+    def main(self):
+        self.common_point()
+        width = self.width()
+        can_width = 5.93
+        accuracy = width/can_width
+        print(f"Accuracy: {accuracy}%")
+        depth = self.depth()
+        accuracy_depth = can_width/depth
+        print(f"Accuracy: {accuracy_depth}%")
+        self.height()
+        
+
+
 
 
 if __name__ == "__main__":
-    app = Tools()
+    app = Dimensions()
