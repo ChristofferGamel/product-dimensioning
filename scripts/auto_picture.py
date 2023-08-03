44cf5d80194eec75cf93a6e3715fdd79ad7a9f0b
ChristofferGamel <christoffer.gamel@gmail.com>
2023-08-03 08:35:35 +0200

    tests

---
+++ scripts/auto_picture.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import math
+
+
+class Await():
+    def __init__(self) -> None:
+        
+        
+        
+        
+        pass
+
+
+    
--- scripts/contoured_image.py
+++ scripts/contoured_image.py
@@ -5,18 +5,18 @@ import math
 class Mask():
     def __init__(self) -> None:
         # Image properties
-        image_path = "./captured_images/baggage_carrier.jpg"
+        image_path = "./captured_images/cup.jpg"
         self.image = cv2.imread(image_path)
         self.image_height = self.image.shape[0]
         self.image_width = self.image.shape[1]
         self.image = self.ResizeWithAspectRatio(self.image, height=700)
 
         # Image adjustments:
-        self.alpha = 1.502         # contrast
-        self.beta = -100           # contrast brightness
+        self.alpha = 1.502          # contrast
+        self.beta = -100            # contrast brightness
         self.kernel_size = 4        # erosion
         self.kernel_iterations = 4  # erosion
-        self.blocksize = 19
+        self.blocksize = 19         # thresholding
         self.C = 7                  # thresholding
         
         self.show()
