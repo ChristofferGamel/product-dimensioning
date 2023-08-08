1cd185a8b6b5e947107222c542986cd3fe555041
Chris <christoffer.gamel@gmail.com>
2023-08-08 19:35:50 +0200

    minor adjustments

--- tools/libcam.py
+++ tools/libcam.py
@@ -3,10 +3,11 @@ from picamera2 import Picamera2, Preview
 
 picam = Picamera2()
 # picam.create_preview_configuration()
-controls = {"ExposureTime": 2100, 
+controls = {"ExposureTime": 1400, 
             "AnalogueGain": 1.0, 
             "Brightness": 0,
-            "Sharpness":50
+            "Sharpness":2,
+            "AwbMode":5
             }
 config = picam.create_preview_configuration(main={"size": (2304, 1296)}, controls=controls)
 #config2 = picam.set_controls({"ExposureTime": 10000, "AnalogueGain": 1.0})
