73cefd5ca0b9bb70f7da5ecd9c584837e5cd85bb
Chris <christoffer.gamel@gmail.com>
2023-08-10 16:00:27 +0200

    libcam as a class

--- tools/libcam.py
+++ tools/libcam.py
@@ -1,26 +1,29 @@
 import time, libcamera
 from picamera2 import Picamera2, Preview
 
-picam = Picamera2()
-# picam.create_preview_configuration()
-controls = {"ExposureTime": 1600, 
-            "AnalogueGain": 1.2, 
-            "Brightness": 0.08,
-            "Sharpness":3,
-            "AwbMode":5
-            }
-config = picam.create_preview_configuration(main={"size": (2304, 1296)}, controls=controls)
-#config2 = picam.set_controls({"ExposureTime": 10000, "AnalogueGain": 1.0})
-picam.configure(config)
-time.sleep(2)
+class Cam():
+    def __init__(self, title) -> None:
+        self.picture(title)
 
-picam.start()
-time.sleep(2)
+    def picture(self, filename):
+        picam = Picamera2()
+        
+        controls = {"ExposureTime": 1600, 
+                    "AnalogueGain": 1.2, 
+                    "Brightness": 0.08,
+                    "Sharpness":3,
+                    "AwbMode":5
+                    }
+        config = picam.create_preview_configuration(main={"size": (2304, 1296)}, controls=controls)
+        
+        picam.configure(config)
+        
+        time.sleep(2)
 
-# Capture the image with specific settings
-picam.capture_file("red.jpg")
+        picam.start()
 
-picam.close()
+        time.sleep(2)
 
+        picam.capture_file("red.jpg")
 
-
+        picam.close()
