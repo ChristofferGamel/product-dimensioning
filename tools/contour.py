a8fa3c12d8746d7b117bd6003f6c38cd43219a8e
ChristofferGamel <christoffer.gamel@gmail.com>
2023-08-10 17:05:28 +0200

    contouring added as a class

---
+++ tools/contour.py
@@ -0,0 +1,101 @@
+import cv2
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+from rembg import remove
+
+
+class Contoured():
+    def __init__(self, image_path) -> None:
+        # Image properties
+        self.image = cv2.imread(image_path)
+        self.alpha = 1.45          # contrast
+        self.beta = -100            # contrast brightness
+        self.kernel_size = 3       # erosion
+        self.kernel_iterations = 9  # erosion
+        self.blocksize = 9         # thresholding
+        self.C = 5                  # thresholding
+
+        self.image_height = self.image.shape[0]
+        self.image_width = self.image.shape[1]
+        
+        self.main()
+
+    def contrast(self, image):
+        contrast = cv2.convertScaleAbs(image, alpha=self.alpha, beta=self.beta)
+        return contrast
+    
+    def remove_bg(self, image):
+        removed_bg = remove(image)
+        return removed_bg
+    
+    def thresholding(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)       
+        blur = cv2.GaussianBlur(gray,(5,5),0)
+        ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        return th3
+    
+    def contour(self, image):
+        image_with_polygon = self.image.copy()
+        contours, hierarchy = cv2.findContours(image=image, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+        
+        largest_contour = max(contours, key=cv2.contourArea)
+        contours_without_largest = [contour for contour in contours if contour is not largest_contour]
+        second_largest_contour = max(contours_without_largest, key=cv2.contourArea)
+        self.find_extremes(second_largest_contour)        
+        cv2.line(image_with_polygon, (self.max_x,self.min_y), (self.max_x,self.max_y), (255, 0, 0), 3)
+        cv2.line(image_with_polygon, (self.min_x,self.max_y), (self.max_x,self.max_y), (255, 0, 0), 3)
+        cv2.line(image_with_polygon, (self.min_x,self.min_y), (self.min_x,self.max_y), (255, 0, 0), 3)
+        cv2.line(image_with_polygon, (self.min_x,self.min_y), (self.max_x,self.min_y), (255, 0, 0), 3)
+        cv2.drawContours(image_with_polygon, contours, -1, (0, 255, 0), thickness=2)
+        
+        for contour in contours:
+            epsilon = 0.14 * cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+            cv2.polylines(image_with_polygon, [approx], True, (0, 255, 0), 2)
+        
+        return image_with_polygon
+    
+    def find_extremes(self, list):
+        self.max_x = 0
+        self.max_y = 0
+        self.min_y = self.image_height
+        self.min_x = self.image_width
+        print(f"Height: {self.image_height}, Width: {self.image_width}")
+        
+        for m_x in range(len(list)):
+            if(list[m_x][0][0] > self.max_x):
+                self.max_x = list[m_x][0][0]
+                self.max_x_set = list[m_x][0]
+        for m_y in range(len(list)):
+            if(list[m_y][0][1] > self.max_y):
+                self.max_y = list[m_y][0][1]
+                self.max_y_set = list[m_y][0]
+
+        for mi_y in range(len(list)):
+            if list[mi_y][0][1] < self.min_y:
+                self.min_y = list[mi_y][0][1]
+                self.min_y_set = list[mi_y][0]
+        for mi_x in range(len(list)):
+            if list[mi_x][0][0] < self.min_x:
+                self.min_x = list[mi_x][0][0]
+                self.min_x_set = list[mi_x][0]
+
+    def main(self):
+        contrasted = self.contrast(self.image)
+        removed_bg = self.remove_bg(contrasted)
+        thresholded = self.thresholding(removed_bg)
+        contoured = self.contour(thresholded)
+        
+        while True:
+            cv2.imshow("contoured", contoured)
+            key = cv2.waitKey(1) & 0xFF
+
+            
+            if key == ord("q"):
+                break
+        cv2.destroyAllWindows()
+        return contoured
+    
+        
+        
