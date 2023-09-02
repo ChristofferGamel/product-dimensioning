9a2cee79e4a1a069594d334926e33dc358e20339
ChristofferGamel <christoffer.gamel@gmail.com>
2023-09-02 10:37:32 +0200

    semaphore, and task_queue added. Needs async help

--- tools/test-files/multithreading.py
+++ tools/test-files/multithreading.py
@@ -1,97 +1,64 @@
 import threading
 import time
 import queue
-import random
 
 class ThreadManager:
     def __init__(self, num_threads):
-        self.threads = [{"thread": threading.Thread(target=self.worker), "is_busy": False} for _ in range(num_threads)]
-        self.q = queue.Queue()
+        self.task_queue = queue.Queue()
         self.chain = QueueManager()
-        self.stop_print_thread = threading.Event()
-        self.print_thread = threading.Thread(target=self.print_results)
-        self.print_thread.start()
+        self.semaphore = threading.Semaphore(num_threads)
+        self.active = True
 
-    def worker(self, input): # Simulating the calculation
-        x = {input: input}
-        time.sleep(4)
-        self.q.put(x)
+        for _ in range(num_threads):
+            threading.Thread(target=self.worker).start()
 
-    def return_queued_value(self):
-        if not self.q.empty():
-            return self.q.get()
-        return None
+    def worker(self):  # Simulating the calculation
+        while self.active or not self.task_queue.empty():
+            input = self.task_queue.get()
+            if input is None:
+                break
 
-    def print_results(self):
-        while not self.stop_print_thread.is_set():
-            result = self.return_queued_value()
-            if result is not None:
-                print(result)
-            time.sleep(0.5)
-    
-    def queued_items(self):
-        length_of_queue = self.chain.queue_length()
-        if length_of_queue == 0:
-            return False
-        elif length_of_queue > 0:
-            return True
-        
+            x = {input: input}
+            time.sleep(4)
+            print(x)
+            self.semaphore.release()
+
     def start_task(self, input):
         self.chain.add_to_queue(input)
-        chain_length = self.chain.queue_length()
-        while chain_length > 0:
-            # threads = self.check_threads()
-            # if threads != None:
-            #     threads["thread"] = threading.Thread(target=self.worker, args=(self.chain.get_first(),))
-            #     threads["thread"].start()
-            #     threads["is_busy"] = True
-            for t in self.threads:
-                if not t["is_busy"]:
-                    t["thread"] = threading.Thread(target=self.worker, args=(self.chain.get_first(),))
-                    t["thread"].start()
-                    t["is_busy"] = True
-                    return True
-            return False
-        
-        
+        self.assign_tasks()
 
-
-    def check_threads(self):
-        for t in self.threads:
-            if t["thread"].is_alive():
-                t["is_busy"] = True
-                return
-            else: 
-                return t
+    def assign_tasks(self):
+        while not self.chain.queue_is_empty() and self.semaphore.acquire(blocking=False):
+            task = self.chain.get_first()
+            self.task_queue.put(task)
 
     def threads_amount(self):
-        self.check_threads()
-        return [t for t in self.threads if not t["is_busy"]]
+        return self.semaphore._value
+
+    def stop(self):
+        self.active = False
+
 
 class QueueManager():
     def __init__(self) -> None:
         self.queue = []
-        pass
 
     def add_to_queue(self, val):
         self.queue.append(val)
-    
+
     def get_first(self):
-        first_element = self.queue[0]
-        self.remove_from_queue(first_element)
+        first_element = self.queue.pop(0)
         return first_element
-    
-    def remove_from_queue(self, element): # Internal Class function
-        self.queue.remove(element)
-        
-    def queue_length(self):
-        return len(self.queue)
+
+    def queue_is_empty(self):
+        return len(self.queue) == 0
+
 
 tm = ThreadManager(2)
 try:
     while True:
         inp = int(input("input: "))
         tm.start_task(inp)
-        print("Unused threads:", len(tm.threads_amount()))
+        print("Unused threads:", tm.threads_amount())
 except KeyboardInterrupt:
-    tm.stop_print_thread.set()
+    tm.stop()
