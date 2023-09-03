79a703d686989a0fef2bcadbe0a330ec81c1753d
ChristofferGamel <christoffer.gamel@gmail.com>
2023-09-03 13:34:21 +0200

    simplified, and works Fix: starts all threads in while loop

--- tools/test-files/multithreading.py
+++ tools/test-files/multithreading.py
@@ -4,61 +4,38 @@ import queue
 
 class ThreadManager:
     def __init__(self, num_threads):
+        self.num_threads = num_threads
         self.task_queue = queue.Queue()
-        self.chain = QueueManager()
         self.semaphore = threading.Semaphore(num_threads)
-        self.active = True
 
-        for _ in range(num_threads):
-            threading.Thread(target=self.worker).start()
+    def start_task(self, inp):
+        self.task_queue.put(inp)
+        while not self.task_queue.empty():
+            for _ in range(self.num_threads):
+                if self.semaphore.acquire(blocking=False):  # Try acquiring without blocking
+                    threading.Thread(target=self.worker).start()
+                else:
+                    time.sleep(0.1)  # If no semaphore available, sleep a bit
 
-    def worker(self):  # Simulating the calculation
-        while self.active or not self.task_queue.empty():
+    def worker(self):
+        try:
             input = self.task_queue.get()
-            if input is None:
-                break
-
             x = {input: input}
             time.sleep(4)
-            print(x)
+            print(x) 
+        finally:  # To ensure the semaphore is always released, even if an exception occurs
             self.semaphore.release()
-
-    def start_task(self, input):
-        self.chain.add_to_queue(input)
-        self.assign_tasks()
-
-    def assign_tasks(self):
-        while not self.chain.queue_is_empty() and self.semaphore.acquire(blocking=False):
-            task = self.chain.get_first()
-            self.task_queue.put(task)
 
     def threads_amount(self):
         return self.semaphore._value
 
-    def stop(self):
-        self.active = False
-
-
-class QueueManager():
-    def __init__(self) -> None:
-        self.queue = []
-
-    def add_to_queue(self, val):
-        self.queue.append(val)
-
-    def get_first(self):
-        first_element = self.queue.pop(0)
-        return first_element
-
-    def queue_is_empty(self):
-        return len(self.queue) == 0
-
-
 tm = ThreadManager(2)
 try:
     while True:
+        print("Unused threads1:", tm.threads_amount())
         inp = int(input("input: "))
         tm.start_task(inp)
-        print("Unused threads:", tm.threads_amount())
+        print("Unused threads2:", tm.threads_amount())
+
 except KeyboardInterrupt:
-    tm.stop()
+    pass  # This will just exit the loop and end the program. `tm.stop()` method does not exist in the provided code.
