7aac931e5a67fdbcf66977707e8749102200591a
Chris <christoffer.gamel@gmail.com>
2023-10-25 15:59:40 +0200

    (r) server setup

--- app/server.py
+++ app/server.py
@@ -1,10 +1,8 @@
-# Needs to be run with sudo
-
 import cv2
 import numpy as np
 from flask import Flask, Response
 from multithreading import ThreadManager
-# from main import Mask
+import main
 
 tm = ThreadManager(2)
 app = Flask(__name__)
@@ -13,21 +11,21 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/img')
-def serve_image():
-    img = np.zeros((512, 512, 3), np.uint8)
-    cv2.circle(img, (256, 256), 50, (0, 255, 0), -1)
+#@app.route('/img')
+#def serve_image():
+#    img = np.zeros((512, 512, 3), np.uint8)
+#    cv2.circle(img, (256, 256), 50, (0, 255, 0), -1)
+#
+#    _, buffer = cv2.imencode('.jpg', img)
+#    jpg_as_text = buffer.tobytes()
+#
+#    response = Response(jpg_as_text, content_type='image/jpeg')
+#    return response
 
-    _, buffer = cv2.imencode('.jpg', img)
-    jpg_as_text = buffer.tobytes()
-
-    response = Response(jpg_as_text, content_type='image/jpeg')
-    return response
-
-# @app.reoute('/get-dimensions/<input>')
-# def serve_dimensions(input):
-#     print("Input: ",input)
-#     width, depth, height = Mask()
+@app.route('/get-dimensions/<input>')
+def serve_dimensions(input):
+    print("Input: ",input)
+    width, depth, height = main.Mask()
 
 @app.route('/<input>')
 def serve_input(input):
@@ -35,4 +33,4 @@ def serve_input(input):
     print(output)
     return output
 
-app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0', port=5000)
