529e2726e52c7ee7a2d57c1bc9059c8c93104b9b
Christoffer Gamél <christoffer.gamel@gmail.com>
2023-10-26 14:03:02 +0200

    draft for queue system

--- app/server.py
+++ app/server.py
@@ -1,10 +1,18 @@
 import cv2
 import numpy as np
 from flask import Flask, Response
-from multithreading import ThreadManager
 from main import Mask
+import queue
+import threading
+import time
 
-tm = ThreadManager(2)
+# Queues
+awaiting_picture = queue.Queue()
+awaiting_processing = queue.Queue()
+
+camera_lock = threading.Lock() # Prevents overlapping picture taking
+picture_taking_in_progress = False
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,18 +22,42 @@ def hello_world():
 @app.route('/get-dimensions/<input>')
 def serve_dimensions(input):
     print("Calculating for: ",input)
-    #dict = Mask().triangulate(input)
-    pictures = take_pictures(input)
+    awaiting_picture.put(input)
+
+
+    pictures = picture_loop()
+    # pictures = take_pictures()
     dict = process_images(pictures)
     return dict
+
+def picture_loop():
+    global picture_taking_in_progress
+
+    if not picture_taking_in_progress:
+        with camera_lock:
+            picture_taking_in_progress = True
+            input = awaiting_picture.get()
+            pictures = take_pictures(input)
+            awaiting_picture.task_done()
+            awaiting_processing.put(pictures)
+            picture_taking_in_progress = False
+            return pictures
+    else:
+        print("Picture lock")
+        time.sleep(0.3)
+        picture_loop()
+        
 
 def take_pictures(id):
     pictures_dict = Mask().take_pictures(id)
     return pictures_dict
 
 def process_images(pictures):
-    result = Mask().triangulate(pictures)
-    return result
+    while not awaiting_processing.empty():
+        pictures = awaiting_processing.get()
+        result = Mask().triangulate(pictures)
+        awaiting_processing.task_done()
+        return result
 
 if __name__ == '__main__':
     app.run(threaded=True)
