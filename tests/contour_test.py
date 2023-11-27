0db716532410c2bcb0c76edfba4831c2ead8595a
Christoffer Gamél <christoffer.gamel@gmail.com>
2023-11-27 22:58:30 +0100

    e2e test for contour

--- tests/contour_test.py
+++ tests/contour_test.py
@@ -1,6 +1,9 @@
 import unittest
 from app.contour import Contoured
 import cv2
+import numpy as np
+import random
+
 
 
 class TestContourClass(unittest.TestCase):
@@ -13,8 +16,29 @@ class TestContourClass(unittest.TestCase):
         point = 360
         fov = 78
         angle = contour.angle(point, fov, 720)
-        print(angle)
-    
+        self.assertTrue(angle == 0)
+
+    def test_e2e(self):
+        height, width = 300, 400  # You can adjust the size of the image
+        image = np.zeros((height, width, 3), dtype=np.uint8)
+
+        left = random.randint(0, width-1)
+        right = random.randint(left, width)
+        top = random.randint(0, height-1)
+        bottom = random.randint(top, height)
+
+        box_color = (255, 255, 255) 
+        box_start_point = (left, top)
+        box_end_point = (right, bottom)
+
+        cv2.rectangle(image, box_start_point, box_end_point, box_color, thickness=-1)
+
+        contour = Contoured(image)
+        contoured = contour.contoured()
+        properties = contour.properties()
+        print(left == properties["left"], left, properties["left"])
+        cv2.imwrite('contoured.jpg',contoured)
+
 
 
 if __name__ == '__main__':
