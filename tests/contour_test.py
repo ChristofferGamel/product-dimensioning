79cdbbbc15f3b18268928a465679a559fcd190b7
Christoffer Gamél <christoffer.gamel@gmail.com>
2023-11-28 19:40:01 +0100

    testing with image examples

--- tests/contour_test.py
+++ tests/contour_test.py
@@ -3,8 +3,9 @@ from app.contour import Contoured
 import cv2
 import numpy as np
 import random
+import os
 
-
+# python -m unittest tests/contour_test.py
 
 class TestContourClass(unittest.TestCase):
 
@@ -38,6 +39,48 @@ class TestContourClass(unittest.TestCase):
         properties = contour.properties()
         print(left == properties["left"], left, properties["left"])
         cv2.imwrite('contoured.jpg',contoured)
+
+
+    def test_e2e(self):
+        height, width = 300, 400
+        image = np.zeros((height, width, 3), dtype=np.uint8)
+
+        left = random.randint(0, width-1)
+        right = random.randint(left, width)
+        top = random.randint(0, height-1)
+        bottom = random.randint(top, height)
+
+        box_color = (255, 255, 255) 
+        box_start_point = (left, top)
+        box_end_point = (right, bottom)
+
+        cv2.rectangle(image, box_start_point, box_end_point, box_color, thickness=-1)
+
+        cv2.imwrite('tests/e2e_pictures/org_image.png',image)
+        contour = Contoured(image)
+        contrasted = contour.contrast(image)
+        cv2.imwrite('tests/e2e_pictures/contrasted.png',contrasted)
+        removed_bg = contour.remove_bg(contrasted)
+        cv2.imwrite('tests/e2e_pictures/removed_bg.png',removed_bg)
+        thresholded = contour.thresholding(removed_bg)
+        cv2.imwrite('tests/e2e_pictures/thresholded.png',thresholded)
+        y1, y2, x1, x2 = contour.extreme_points(thresholded)
+        draw = contour.draw_points_box(image, y1, y2, x1, x2)
+        cv2.imwrite('tests/e2e_pictures/draw.png',draw)
+    
+    def test_image_examples(self):
+        directory = 'tests/test_pictures'
+        index = 0
+        for filename in os.listdir(directory):
+            print(filename)
+            f = os.path.join(directory, filename)
+            image = cv2.imread(f)
+            contour = Contoured(image)
+            contoured = contour.contoured()
+            cv2.imwrite(f'tests/contoured_pictures/{index}.png',contoured)
+            index += 1
+
+                
 
 
 
